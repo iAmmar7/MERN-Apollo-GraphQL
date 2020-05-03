60891bb2576cd21e32481588fa9ad443cf43cdfe
Ammar <maansaridot@gmail.com>
2020-05-03 10:40:01 +0500

    heroku deploy

--- server.js
+++ server.js
@@ -29,15 +29,11 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app, cors: false });
 
-// Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //Set status folder
-  app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 4000;
 
